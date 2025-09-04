6af9351bc182c0281a2d63d9610c34d4323e6524
Kaese72 <charliegen@hotmail.se>
2025-09-05 01:48:16 +0200

    allowing some more empty fields

--- ingestmodels/group.go
+++ ingestmodels/group.go
@@ -2,7 +2,7 @@ package ingestmodels
 
 type IngestGroup struct {
 	Name             string                  `json:"name"`
-	BridgeKey        string                  `json:"bridge-key"`
+	BridgeKey        string                  `json:"bridge-key" required:"false"`
 	BridgeIdentifier string                  `json:"bridge-identifier"`
 	Capabilities     []IngestGroupCapability `json:"capabilities"`
 	DeviceIds        []int                   `json:"device-ids"`
