9c597a095cfbb420cb684c8f0296b84f96a486ff
Kaese72 <charliegen@hotmail.se>
2025-09-05 01:40:32 +0200

    Bridge-Key is not required

--- ingestmodels/device.go
+++ ingestmodels/device.go
@@ -2,7 +2,7 @@ package ingestmodels
 
 type IngestDevice struct {
 	BridgeIdentifier string                   `json:"bridge-identifier"`
-	BridgeKey        string                   `json:"bridge-key"`
+	BridgeKey        string                   `json:"bridge-key" required:"false"`
 	Attributes       []IngestAttribute        `json:"attributes"`
 	Capabilities     []IngestDeviceCapability `json:"capabilities"`
 	Triggers         []IngestTrigger          `json:"triggers"`
