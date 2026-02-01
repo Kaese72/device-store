81b0ab272fdb7d5cbf198d4f90f4b759adc58a3a
Kaese72 <charliegen@hotmail.se>
2026-02-01 16:31:47 +0100

    removed cascade from delete

--- migrations/V011.sql
+++ migrations/V011.sql
@@ -1,10 +1,10 @@
-DELETE FROM devices CASCADE;
+DELETE FROM devices;
 ALTER TABLE devices DROP INDEX unique_identifier_per_bridge;
 ALTER TABLE devices DROP COLUMN bridgeKey;
 ALTER TABLE devices ADD COLUMN adapterId BIGINT UNSIGNED NOT NULL;
 ALTER TABLE devices ADD CONSTRAINT unique_identifier_per_bridge UNIQUE (bridgeIdentifier, adapterId);
 
-DELETE FROM groups CASCADE;
+DELETE FROM groups;
 ALTER TABLE groups DROP INDEX unique_identifier_per_bridge;
 ALTER TABLE groups DROP COLUMN bridgeKey;
 ALTER TABLE groups ADD COLUMN adapterId BIGINT UNSIGNED NOT NULL;
